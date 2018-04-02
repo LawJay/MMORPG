fac329be8ca049894d54d2551d8c28083c67b6e8
Jamie Law <jamiepeterlaw@gmail.com>
2018-04-02 21:15:59 +0100

    session 1

---
+++ Sys/config.php
@@ -0,0 +1,12 @@
+<?php
+$title 				= "MMORTS";
+$seperator 			= " - ";
+$description 		= "My awesome MMORTS game.";
+
+$maintenance 		= false;
+
+$dbserver 			= "";
+$dbusername 		= "";
+$dbpassword 		= "";
+$db 				= "";
+?>
