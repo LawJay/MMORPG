db1b6191dca3ee601d9a7e5bb5baaddd71323fc7
Jamie Law <jamiepeterlaw@gmail.com>
2018-04-02 20:39:24 +0100

    init

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 4/2/2018
+ * Time: 8:31 PM
+ */
+$maintenance == false;
+if($maintenance == true){
+    echo "The site is currently unavaliable";
+}
+elseif($maintenance == false){
+    echo "site";
+}
